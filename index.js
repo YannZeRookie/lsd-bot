ff02568301b42912f826c0f838241c154b5bfc5c
YannZeRookie <yannzerookie@gmail.com>
2019-11-24 00:23:24 +0100

    Refactoring of code + clean-up connection messages

--- index.js
+++ index.js
@@ -28,61 +28,47 @@ if (mysqlConfig.host) {
     console.log('Running in no database mode');
 }
 
+
 /**
- * Return help message
+ * General listening entry point
  */
-function helpMessage()
-{
-    return "Bonjour, je suis le Bot des Scorpions du Désert. Les commandes commencent par `!`\nVoici la liste : \n\
-      ```\n\
-!inscription          Poste ta candidature pour devenir un ou une LSD !\n\
-!connexion            Connecte-toi sur le site de gestion de ton compte LSD\n\
-!aide, !help, !sos    Obtenir cette aide\n\
-!lance nombre         Lance un dé entre 1 et 'nombre'. Par exemple pour un dé à 6 faces : !lance 6\n\
-      ```";
-}
-
-discordBot.hears('^!connexion','ambient',(bot, msg) => {
-	//console.log(util.inspect(bot));
-	//console.log(util.inspect(msg));
+discordBot.hears('^' + config.prefix + '.*','ambient',(bot, msg) => {
     if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    var key = buildConnectionKey(msg.message.author);
-    msg.message.author.send("Voici ton lien de connexion : " + buidLoginUrl(key));
-    //bot.reply(msg, 'Je vous ai transmis un lien de connexion par message privé dans Discord');
-    console.log('Connection request from ' + msg.message.author.username + ' (' + msg.message.author.id + ')');
-});
-
-discordBot.hears('^!inscription','ambient',(bot, msg) => {
-    if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    var key = buildConnectionKey(msg.message.author);
-    msg.message.author.send("Voici ton lien pour t'inscrire : " + buidLoginUrl(key));
-    console.log('Inscription request from ' + msg.message.author.username + ' (' + msg.message.author.id + ')');
+    var words = msg.message.content.substring(1).split(' ');
+    if (words.length) {
+        processCommand(words[0], 'ambient', bot, msg);
+    }
 });
 
 discordBot.hears('hello','ambient',(bot, msg) => {
 	//console.log(util.inspect(bot));
     //console.log(util.inspect(msg));
     if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    bot.reply(msg, 'Salut à toi ' + msg.message.author.username + ' ! Pour avoir de l\'aide, tape `!aide`');
-    //console.log('Replied to hello from ' + msg.message.author.username+ ' msg: ' + msg.message.content);
-    //console.log("\n");
+    processCommand('hello', 'ambient', bot, msg);
 });
 
+/**
+ * Direct Message = private message sent to the Bot
+ */
 discordBot.hears('.*', 'direct_message', (bot, msg) => {
-	//console.log(util.inspect(bot));
-    //console.log(util.inspect(message));
     if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    bot.reply(msg.message, helpMessage());
-    //console.log('Received a direct_message from ' + msg.message.author.username);
-    //console.log("\n");    
+    // Remove the prefix, if any (we assume that all input are commands)
+    var message = (msg.message.content[0] == config.prefix) ? msg.message.content.substring(1) : msg.message.content;
+    var words = message.split(' ');
+    if (words.length) {
+        processCommand(words[0], 'direct_message', bot, msg);
+    }
 });
 
+/**
+ * Direct mention = mentionning the Bot during a private message to the Bot
+ */
 discordBot.hears('.*', 'direct_mention', (bot, msg) => {
 	//console.log(util.inspect(bot));
 	//console.log(util.inspect(msg));
     if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
     //bot.reply(msg.message, 'Received a direct_mention from ' + msg.message.author.username);
-    bot.reply(msg, "Salut " + msg.message.author.username + ", si tu as besoin d'aide, tape `!aide`");
+    bot.reply(msg, "Salut " + msg.message.author.username + ", si tu as besoin d'aide, tape `"+config.prefix+"aide`");
     /*
     bot.send({
     	text: "You're talking to me?",
@@ -93,53 +79,15 @@ discordBot.hears('.*', 'direct_mention', (bot, msg) => {
     //console.log("\n");
 });
 
+/**
+ * Mention = mentioning the Bot in a general channel
+ */
 discordBot.hears('.*', 'mention', (bot, msg) => {
-	//console.log(util.inspect(bot));
-	//console.log(util.inspect(msg));
     if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    //bot.reply(msg.message, 'Received a mention from ' + msg.message.author.username);
-    bot.reply(msg, msg.message.author.username + " parle de moi, c'est sympa ! Pour avoir de l'aide, tape `!aide`");
+    bot.reply(msg, msg.message.author.username + " parle de moi, c'est sympa ! Pour avoir de l'aide, tape `"+config.prefix+"aide`");
     //console.log('Was mentioned by ' + msg.message.author.username);
     //console.log("\n");
 });
-
-discordBot.hears('^!aide', 'ambient', (bot, msg) => {
-    if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    bot.reply(msg, helpMessage());
-});
-
-discordBot.hears('^!sos', 'ambient', (bot, msg) => {
-    if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    bot.reply(msg, helpMessage());
-});
-
-discordBot.hears('^!help', 'ambient', (bot, msg) => {
-    if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    bot.reply(msg, helpMessage());
-});
-
-
-discordBot.hears('^!lance .+', 'ambient', (bot, msg) => {
-    //console.log(util.inspect(bot));
-    //console.log(util.inspect(msg));
-    if (msg.message.author.id == discordConfig.client.user.id) return; // Don't answer to ourselves
-    var r = msg.message.content.match(/lance\s+(\d+)/);
-    if (r && r[1] && r[1]>1) {
-        if (r[1]<=100000000) {
-            var result = 1 + Math.floor(Math.random() * Math.floor(r[1]));
-            bot.reply(msg.message, 'OK, je lance un dé à ' + r[1] + ' faces ! Résultat : ' + result);
-            //console.log(msg.message.author.username + ' threw a dice! faces=' + r[1] + ' result=' + result);
-        }
-        else {
-            bot.reply(msg.message, "Désolé, je suis limité à 100000000");
-        }
-    } else if (msg.message.content.match(/lance\s+.*Totor0/i)) {
-        bot.reply(msg.message, "Désolé, je ne pratique pas le lancer de nains !");
-    } else {
-        bot.reply(msg.message, "Désolé, je n'ai pas compris. Il me faut un nombre ≥ 2 après la commande");
-    }
-});
-
 
 discordBot.on('ready', (bot, event) => {
     bot.log('Ready!');
@@ -151,6 +99,53 @@ discordBot.on('disconnect', (bot, event) => {
     }
     bot.log('Good-bye!');
 });
+
+/**
+ * General command processing dispatcher
+ * @param {string} command 
+ * @param {string} context 
+ * @param {*} bot 
+ * @param {*} msg 
+ */
+function processCommand(command, context, bot, msg)
+{
+    switch(command) {
+        case 'connexion':
+        case 'connection':
+        case 'connect':
+        case 'login':
+            var key = buildConnectionKey(msg.message.author);
+            msg.message.author.send("Voici ton lien de connexion : " + buidLoginUrl(key));
+            console.log('Connection request from ' + msg.message.author.username + ' (' + msg.message.author.id + ')');
+            if (context == 'ambient') {
+                msg.message.delete(4000);   // Remove the message to avoid poluting the channel
+            }
+            break;
+        case 'inscription':
+        case 'signup':
+            var key = buildConnectionKey(msg.message.author);
+            msg.message.author.send("Voici ton lien pour t'inscrire : " + buidLoginUrl(key));
+            console.log('Inscription request from ' + msg.message.author.username + ' (' + msg.message.author.id + ')');
+            if (context == 'ambient') {
+                msg.message.delete(4000);   // Remove the message to avoid poluting the channel
+            }
+            break;
+        case 'hello':
+            bot.reply(msg, 'Salut à toi ' + msg.message.author.username + ' ! Pour avoir de l\'aide, tape `' + config.prefix + 'aide`');
+            break;
+        case 'lance':
+            lance(bot, msg);
+            break;
+        case 'help':
+        case 'aide':
+        case 'sos':
+            bot.reply(msg, helpMessage());
+            break;
+        default:
+            bot.reply(msg, "Commande inconnue, tape `"+config.prefix+"aide` pour la liste des commandes disponibles");
+
+    }
+}
 
 function buildConnectionKey(user)
 {
@@ -173,4 +168,41 @@ function buildConnectionKey(user)
 function buidLoginUrl(key)
 {
     return config.connection_url + '/' + key;
+}
+
+/**
+ * Return help message
+ */
+function helpMessage()
+{
+    return "Bonjour, je suis le Bot des Scorpions du Désert. Les commandes commencent par `"+config.prefix+"`\nVoici la liste : \n\
+      ```\n\
+"+config.prefix+"inscription          Poste ta candidature pour devenir un ou une LSD !\n\
+"+config.prefix+"connexion            Connecte-toi sur le site de gestion de ton compte LSD\n\
+"+config.prefix+"aide, "+config.prefix+"help, "+config.prefix+"sos    Obtenir cette aide\n\
+"+config.prefix+"lance nombre         Lance un dé entre 1 et 'nombre'. Par exemple pour un dé à 6 faces : "+config.prefix+"lance 6\n\
+      ```";
+}
+
+/**
+ * Random dice generator
+ * @param {*} bot 
+ * @param {*} msg 
+ */
+function lance(bot, msg)
+{
+    var r = msg.message.content.match(/lance\s+(\d+)/);
+    if (r && r[1] && r[1]>1) {
+        if (r[1]<=100000000) {
+            var result = 1 + Math.floor(Math.random() * Math.floor(r[1]));
+            bot.reply(msg.message, 'OK, je lance un dé à ' + r[1] + ' faces ! Résultat : ' + result);
+        }
+        else {
+            bot.reply(msg.message, "Désolé, je suis limité à 100000000");
+        }
+    } else if (msg.message.content.match(/lance\s+.*Totor0/i)) {
+        bot.reply(msg.message, "Désolé, je ne pratique pas le lancer de nains !");
+    } else {
+        bot.reply(msg.message, "Désolé, je n'ai pas compris. Il me faut un nombre ≥ 2 après la commande");
+    }
 }
