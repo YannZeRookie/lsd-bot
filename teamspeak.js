5cb5c33c8229f62e5fc4affce8e869870c2b8dda
Yann Corno <yannzerookie@gmail.com>
2021-03-06 13:58:33 +0100

    TeamSpeak: complete new connection paradigm: temporary connection
    We login, generate the token, then logout.

--- teamspeak.js
+++ teamspeak.js
@@ -6,93 +6,45 @@
  */
 
 const ts_lib = require('ts3-nodejs-library');
-var isConnected = false;
-
 
 // Load TS configuration settings
 const ts_config = require('./teamspeak.json');
 
-// Connect
-const teamspeak = new ts_lib.TeamSpeak(ts_config);
+/*
+const me = await teamspeak.whoami();
+console.log(me);
+*/
 
-teamspeak.on("ready", async () => {
-    try {
-        //-- Connection
-        console.log("TeamSpeak: LSD-Bot Connected to TeamSpeak server");
-        //-- One-time initialization
-
-        /*
-        const me = await teamspeak.whoami();
-        console.log(me);
-        */
-
-        /*
-        console.log('TeamSpeak: channels:');
-        const channels = await teamspeak.channelList();
-        channels.forEach(channel => {
-            console.log(channel);
-        });
-        */
-
-        /*
-        console.log('TeamSpeak: server groups:');
-        const groups = await teamspeak.serverGroupList();
-        groups.forEach(group => {
-            console.log(group);
-        });
-        */
-
-        /*
-        await teamspeak.sendTextMessage("14132392", 2, "LSD-Bot connecté");
-        */
-        isConnected = true;
-    } catch (error) {
-        console.error("TeamSpeak: ready Error: " + error);
-    }
+/*
+console.log('TeamSpeak: channels:');
+const channels = await teamspeak.channelList();
+channels.forEach(channel => {
+    console.log(channel);
 });
+*/
 
-//-- Disconnected: try to reconnect
-teamspeak.on("close", async () => {
-    try {
-        isConnected = false;
-        console.log("TeamSpeak: LSD-Bot disconnected")
-        //await teamspeak.reconnect(-1, 1000);
-        //console.log("TeamSpeak: LSD-Bot reconnected!")
-    } catch (error) {
-        console.error("TeamSpeak: close Error: " + error);
-    }
+/*
+console.log('TeamSpeak: server groups:');
+const groups = await teamspeak.serverGroupList();
+groups.forEach(group => {
+    console.log(group);
 });
+*/
 
-//-- Teamspeak had an error
-teamspeak.on("error", async (e) => {
-    try {
-        console.error("TeamSpeak: Error: " + e);
-    } catch (error) {
-        console.error("TeamSpeak: Error: " + error);
-    }
-});
-
-//-- Client connected
-teamspeak.on("clientconnect", async (event) => {
-    try {
-        const client = event.client;
-        console.log("TeamSpeak: Client connected: " + event);
-    } catch (error) {
-        console.error("TeamSpeak: clientconnect Error: " + error);
-    }
-});
+/*
+await teamspeak.sendTextMessage("14132392", 2, "LSD-Bot connecté");
+*/
 
 //-- Client sent text message
+/*
 teamspeak.on("textmessage", async (event) => {
     try {
-        /*
         console.log(`TeamSpeak: ${event.invoker.nickname} just send the message "${event.msg}"`)
-        */
     } catch (error) {
         console.error("TeamSpeak: textmessage Error: " + error);
     }
 });
-
+*/
 
 /*
 ts_lib.TeamSpeak.connect(ts_config).then(async teamspeak => {
@@ -106,36 +58,37 @@ ts_lib.TeamSpeak.connect(ts_config).then(async teamspeak => {
 */
 
 async function getConnectionLink(roles, target) {
+    var result = '';
     try {
-        if (!isConnected) {
-            await teamspeak.reconnect(1, 1000);
-        }
-    } catch (error) {
-        console.error(error);
+        await ts_lib.TeamSpeak.connect(ts_config).then(async teamspeak => {
+            console.log("TeamSpeak: LSD-Bot connected to TeamSpeak server");
+            try {
+                var sgid = '4363725';   // Invité server group id is default
+                if (roles.some(role => role.name === 'Conseiller')) {
+                    sgid = '4365577';
+                } else if (roles.some(role => role.name === 'Officier')) {
+                    sgid = '4365576';
+                } else if (roles.some(role => role.name === 'Scorpion')) {
+                    sgid = '4363724';
+                }
+                const key = await teamspeak.privilegeKeyAdd(0, sgid, undefined, 'Clé pour ' + target, '');
+                token = key.token;
+                console.log('TeamSpeak: Generated token ' + token + ' for ' + target);
+                if (token) {
+                    result = 'ts3server://' + ts_config.host + '?port=' + ts_config.serverport + '&token=' + token;
+                }
+                await teamspeak.logout();
+                console.log("TeamSpeak: LSD-Bot disconnected from TeamSpeak server");
+            } catch (error) {
+                console.error("TeamSpeak: getConnectionLink: " + error);
+            }
+        }).catch(e => {
+            console.error("TeamSpeak: connection error: " + e);
+        });
+    } catch (ee) {
+        console.error("TeamSpeak: init error: " + ee);
     }
-    var token = '';
-    try {
-        var sgid = '4363725';   // Invité server group id is default
-        if (roles.some(role => role.name === 'Conseiller')) {
-            sgid = '4365577';
-        } else if (roles.some(role => role.name === 'Officier')) {
-            sgid = '4365576';
-        } else if (roles.some(role => role.name === 'Scorpion')) {
-            sgid = '4363724';
-        }
-
-        const key = await teamspeak.privilegeKeyAdd(0, sgid, undefined, 'Clé pour ' + target, '');
-        token = key.token;
-        console.log('Generated token ' + token + ' for ' + target);
-    } catch (error) {
-        console.error(error);
-    }
-    if (token) {
-        return 'ts3server://' + ts_config.host + '?port=' + ts_config.serverport + '&token=' + token;
-    }
-    else {
-        return 'ts3server://' + ts_config.host + '?port=' + ts_config.serverport;
-    }
+    return result;
 }
 
 
